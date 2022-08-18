2f81972e616e7091282035df40386885ff9dcd92
Francisco Caballero <fjcero@gmail.com>
2022-08-18 15:50:17 -0300

    feat: Update pexels lib

--- libs/pexels.ts
+++ libs/pexels.ts
@@ -1,4 +1,11 @@
-export const getCurated = async (page: number, perPage: number) =>
-  await fetch(
-    `https://api.pexels.com/v1/curated?page=${page}&per_page=${perPage}`
-  ).then((result) => result.json());
+import { createClient } from "pexels";
+
+export const pexels = () => {
+  const apiKey = process.env.NEXT_PUBLIC_PEXELS_API_KEY;
+  return apiKey ? createClient(apiKey) : null;
+};
+
+export const getCurated = async (page: number, perPage: number) => {
+  const client = pexels();
+  return await client?.photos.curated({ page, per_page: perPage });
+};
